64077a7caf455dee65edf69db42120d8c7f139fe
snoringarvind <snoringarvind@gmail.com>
2021-01-27 18:59:21 +0530

    .

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import List from "./List";
 import Navigation from "../Navigation/Navigation";
 import AuthButton from "../AuthButton";
